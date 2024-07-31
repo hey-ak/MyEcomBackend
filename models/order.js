04412876dcd7a77d8094326ab55758a8c54ba1ed
hey-ak <akshay0127.be21@chitkara.edu.in>
2024-08-01 01:06:51 +0530

    timestamp added in order

--- models/order.js
+++ models/order.js
@@ -1,18 +1,21 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  quantity: Number,
-  address: String,
-  mobileNo: String,
-  pincode: String,
-  paymentMethod: String,
-  country: String,
-  status: {
-    type: String,
-    default: "Placed",
+const orderSchema = new mongoose.Schema(
+  {
+    quantity: Number,
+    address: String,
+    mobileNo: String,
+    pincode: String,
+    paymentMethod: String,
+    country: String,
+    status: {
+      type: String,
+      default: "Placed",
+    },
+    userId: mongoose.Schema.Types.ObjectId,
+    productId: mongoose.Schema.Types.ObjectId,
   },
-  userId: mongoose.Schema.Types.ObjectId,
-  productId: mongoose.Schema.Types.ObjectId,
-});
+  { timestamps: true }
+);
 const Order = mongoose.model("Order", orderSchema);
 module.exports = Order;
